c61baa6e48f3ae409cb3f54fd29f58886a046f83
郭志锋 <87801441+togekel@users.noreply.github.com>
2024-12-22 23:59:03 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.feng = import ./home-manager/home.nix;
+          home-manager.users.feng = import ./home.nix;
         }
       ];
     };

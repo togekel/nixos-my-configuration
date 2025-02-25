975e6d4a294155583717bbf261d49dba036ea129
Togekel <1274911994@qq.com>
2025-02-25 22:19:24 +0800

    update

--- home.nix
+++ home.nix
@@ -96,13 +96,7 @@ in
     # wpsoffice-cn # This version is old. Use Nur.
     # jetbrains.clion # CLion for C Dev.
     nur.repos.novel2430.wemeet-bin-bwrap-wayland-screenshare
-    nur.repos.novel2430.wpsoffice-cn.overrideAttrs (finalAttrs: previousAttrs: {
-      shellHook = ''
-        export GTK_IM_MODULE=fcitx
-        export QT_IM_MODULE=fcitx5
-        export XMODIFIERS=@im=fcitx
-      '';
-    })  
+    nur.repos.novel2430.wpsoffice-cn
   ]) ++ (with pkgs.gnomeExtensions; [
     kimpanel
     dash-to-dock

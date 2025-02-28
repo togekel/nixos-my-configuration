da5e2a896de445bc4386911dc075754d944dc958
Togekel <1274911994@qq.com>
2025-02-28 14:39:35 +0800

    update

--- configuration.nix
+++ configuration.nix
@@ -189,14 +189,14 @@
 	};
 
 	# Install the driver
-	services.fprintd.enable = true;
+	# services.fprintd.enable = true;
 	# If simply enabling fprintd is not enough, try enabling fprintd.tod...
-	services.fprintd.tod.enable = true;
+	# services.fprintd.tod.enable = true;
 	# ...and use one of the next four drivers
 	# services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix; # Goodix driver module
 	# services.fprintd.tod.driver = pkgs.libfprint-2-tod1-elan; # Elan(04f3:0c4b) driver
 	# services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090; # driver for 2016 ThinkPads
-	services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix-550a; # Goodix 550a driver (from Lenovo)
+	# services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix-550a; # Goodix 550a driver (from Lenovo)
 
 	# however for focaltech 2808:a658, use fprintd with overidden package (without tod)
 	# services.fprintd.package = pkgs.fprintd.override {

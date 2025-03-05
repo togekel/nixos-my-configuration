7b68f7cf7d212ef385432d94ac74ec826feaffb9
Togekel <1274911994@qq.com>
2025-03-05 10:58:36 +0800

    update

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -11,6 +11,7 @@
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelParams = [ "kvm.enable_virt_at_load=0" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =

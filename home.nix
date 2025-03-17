7b575aa9434b93c6fdfa3bbcae620fbb249b4cf3
Togekel <1274911994@qq.com>
2025-03-17 23:41:25 +0800

    update

--- home.nix
+++ home.nix
@@ -22,6 +22,9 @@ let
   jetbrains-pycharm = pkgs.jetbrains.pycharm-professional.overrideAttrs (finalAttrs: previousAttrs: {
     fromSource = true;
   });
+  zotero-beta-fixed = pkgs.zotero-beta.overrideAttrs (finalAttrs: previousAttrs: {
+  		LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+  	}); # fix libstdc++.so.6 missing error.
 in
 {
   # Home Manager needs a bit of information about you and the
@@ -110,7 +113,7 @@ in
     jetbrains-pycharm # PyCharm for Python Dev.
     nur.repos.novel2430.wemeet-bin-bwrap-wayland-screenshare
     nur.repos.novel2430.wpsoffice-cn # winfonts needed.
-    zotero-beta # zotero somehow meets bugs (missing libstdc++6)
+    zotero-beta-fixed # zotero somehow meets bugs (missing libstdc++6)
     planner # wbs
     super-productivity # todo app
     gimp # image processing
@@ -137,7 +140,6 @@ in
   
   # Set GTK/QT Theme.
   home.sessionVariables.GTK_THEME = "WhiteSur-Light";
-  home.sessionVariables.LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib"; # fix libstdc++.so.6 not found.
   gtk = {
     enable = true;
     iconTheme = {

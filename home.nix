1cdb0556d3b2a5e1ed85dc02c2c6e401da278902
Togekel <1274911994@qq.com>
2025-03-18 15:53:51 +0800

    update

--- home.nix
+++ home.nix
@@ -7,21 +7,24 @@ let
     thefuck
   ]);
   my-python = pkgs.python3.withPackages (p: [
+  		p.pip
     p.numpy
     p.pandas
     p.matplotlib
+    p.scipy
+    p.sympy
   ]);
   dev-tools = (with pkgs; [
     gcc
     gnumake
     cmake
   ]);
-  # jetbrains-clion = pkgs.jetbrains.clion.overrideAttrs (finalAttrs: previousAttrs: {
-  #   fromSource = true;
-  # });
-  # jetbrains-pycharm = pkgs.jetbrains.pycharm-professional.overrideAttrs (finalAttrs: previousAttrs: {
-  #   fromSource = true;
-  # });
+  jetbrains-clion = pkgs.jetbrains.clion.overrideAttrs (finalAttrs: previousAttrs: {
+    fromSource = true;
+  });
+  jetbrains-pycharm = pkgs.jetbrains.pycharm-professional.overrideAttrs (finalAttrs: previousAttrs: {
+    fromSource = true;
+  });
 in
 {
   # Home Manager needs a bit of information about you and the
@@ -107,8 +110,8 @@ in
     wechat-uos
     vscode # VSCode.
     # wpsoffice-cn # This version is old. Use Nur.
-    # jetbrains-clion # CLion for C Dev.
-    # jetbrains-pycharm # PyCharm for Python Dev.
+    jetbrains-clion # CLion for C Dev.
+    jetbrains-pycharm # PyCharm for Python Dev.
     nur.repos.novel2430.wemeet-bin-bwrap-wayland-screenshare
     nur.repos.novel2430.wpsoffice-cn # winfonts needed.
     zotero-beta
@@ -135,7 +138,7 @@ in
   dev-tools; # tools for c dev.
   
   # Fix libstdc++.so.6 missing bug.
-  # home.sessionVariables.LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+  home.sessionVariables.LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
   
   # Set GTK/QT Theme.
   home.sessionVariables.GTK_THEME = "WhiteSur-Light";
